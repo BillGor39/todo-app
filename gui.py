ff413e4c83d54bc574ce2c1d5285254170abd1c0
Edward <hua9939@gmail.com>
2023-12-03 16:15:43 +1300

    improve the try and except with popup window and add clock feature

--- gui.py
+++ gui.py
@@ -1,25 +1,30 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+sg.theme("Black")
+clock_label = sg.Text(key="clock")
 input_label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 todo_label = sg.Text("")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
-                      enable_events=True, size=[44, 10])
+                      enable_events=True, size=[44,10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 message_label = sg.Text(key="message")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-do App",
-                   layout=[[input_label],
+                   layout=[[clock_label],
+                           [input_label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button, message_label]],
                    font=("Helvetica", 20))
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
 
     match event:
         case "Add":
@@ -40,7 +45,8 @@ while True:
                 functions.write_todos(todos)
                 window["todos"].update(values=todos)
             except IndexError:
-                window["message"].update("choose a todo to edit.")
+                # window["message"].update("choose a todo to edit.")
+                sg.popup("Choose a todo to edit.", font=("Helvetica", 20))
 
         case "Complete":
             try:
@@ -51,8 +57,8 @@ while True:
                 window["todo"].update(value="")
                 window["todos"].update(values=todos)
             except IndexError:
-                window["message"].update("choose a todo to complete.")
-
+                # window["message"].update("choose a todo to complete.")
+                sg.popup("Choose a todo to complete.", font=("Helvetica", 20))
         case "todos":
             window["todo"].update(value=values["todos"][0])
 
