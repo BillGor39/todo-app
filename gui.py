a202c5bd457147de045865482a27ad51e0e1a422
Edward <hua9939@gmail.com>
2023-12-03 19:23:22 +1300

    fix the edit bug

--- gui.py
+++ gui.py
@@ -1,19 +1,36 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
+
 
 sg.theme("Black")
 clock_label = sg.Text(key="clock")
 input_label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button(image_size=(50, 30), mouseover_colors="LightBlue2",
-                       tooltip="Add to do", image_source="add.png", key="Add")
+                       tooltip="Add to do", image_source=resource_path("add.png"), key="Add")
 todo_label = sg.Text("")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(44, 10))
 edit_button = sg.Button("Edit")
 complete_button = sg.Button(image_size=(60, 50), mouseover_colors="LightBlue2", tooltip="complete",
-                            image_source="complete.png", key="Complete")
+                            image_source=resource_path("complete.png"), key="Complete")
 message_label = sg.Text(key="message")
 exit_button = sg.Button("Exit")
 
@@ -31,7 +48,7 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["todo"].capitalize() + "\n"
+            new_todo = values["todo"].capitalize().strip(" ") + "\n"
             if new_todo != "\n":
                 todos.append(new_todo)
                 functions.write_todos(todos)
@@ -43,12 +60,16 @@ while True:
         case "Edit":
             try:
                 todo_to_edit = values["todos"][0]
-                new_todo = values["todo"].capitalize() + "\n"
-                todos = functions.get_todos()
-                index = todos.index(todo_to_edit)
-                todos[index] = new_todo
-                functions.write_todos(todos)
-                window["todos"].update(values=todos)
+                new_todo = values["todo"].capitalize().strip(" ") + "\n"
+                if new_todo != "\n":
+                    todos = functions.get_todos()
+                    index = todos.index(todo_to_edit)
+                    todos[index] = new_todo
+                    functions.write_todos(todos)
+                    window["todos"].update(values=todos)
+
+                else:
+                    sg.popup("cannot edit to empty todo.")
             except IndexError:
                 # window["message"].update("choose a todo to edit.")
                 sg.popup("Choose a todo to edit.", font=("Helvetica", 20))
@@ -64,8 +85,12 @@ while True:
             except IndexError:
                 # window["message"].update("choose a todo to complete.")
                 sg.popup("Choose a todo to complete.", font=("Helvetica", 20))
+
         case "todos":
-            window["todo"].update(value=values["todos"][0])
+            try:
+                window["todo"].update(value=values["todos"][0].strip("\n"))
+            except IndexError:
+                continue
 
         case "Exit":
             break
